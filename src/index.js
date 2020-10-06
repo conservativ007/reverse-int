11f5ea98aec8fb206e1a111fefdd95afe6e32708
conservativ007 <lexxar_89@mail.ru>
2020-10-06 15:53:06 +0300

    feat: added task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let arr = n.toString().split('').reverse();
+
+	if(arr[arr.length -1] == '-'){
+		arr.splice([arr.length -1], 1);
+	}
+
+
+  return arr.join('');
 }
+
